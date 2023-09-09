e348ac0948bf872bb25cca4a5bfa3269c3127a86
Oluwafemi Ibitoye <oluwafemiibitoye018@gmail.com>
2023-09-08 21:45:27 -0700

    a program that prints all possible different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int x, y; /*Declaring statement*/
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (y = '0'; y <= '9'; y++)
+		{
+			if (x < y)
+			{
+				putchar(x);
+				putchar(y);
+
+				if (x != '8' || (x == '8' && y != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
