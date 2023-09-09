07b2208d752b5d7dca1451d717facf0b45b5f9cc
Oluwafemi Ibitoye <oluwafemiibitoye018@gmail.com>
2023-09-08 21:40:14 -0700

    a program that prints all the numbers of base 16 in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char norlcbase_16; /*Decarling statement*/
+
+	/*for base 16 0-9*/
+	for (norlcbase_16 = 48; norlcbase_16 <= 57; norlcbase_16++)
+	{
+		putchar(norlcbase_16); /*print 0-9*/
+	}
+
+	/*for base 16 a-f*/
+	for (norlcbase_16 = 97 ; norlcbase_16 <= 102; norlcbase_16++)
+	{
+		putchar(norlcbase_16); /*print a-f*/
+	}
+
+	putchar ('\n'); /*print new line*/
+
+	return (0);
+}
