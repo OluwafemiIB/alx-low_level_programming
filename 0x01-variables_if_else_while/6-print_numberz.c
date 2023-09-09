2ef6481c21a0eae04aceef0d95494b8bd9579553
Oluwafemi Ibitoye <oluwafemiibitoye018@gmail.com>
2023-09-08 21:34:33 -0700

    a program that prints all single digit numbers of base 10 starting from 0, followed by a new line

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	/*Declaring statement*/
+	int i;
+
+	/*for count from 48-57*/
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+
+	/*new line*/
+	putchar('\n');
+
+	return (0);
+}
