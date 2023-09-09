b54d011430e307686a5e9dbecea032d4d0d53b53
Oluwafemi Ibitoye <oluwafemiibitoye018@gmail.com>
2023-09-08 21:42:46 -0700

    a program that prints all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	int num; /*Decarling statement*/
+
+	/*for 0-9 with (,)*/
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num); /*print  number*/
+
+		/*if the value is 9 in format ASCII break the program*/
+		if (num == 57)
+		{
+			break;
+		}
+
+		putchar(','); /*print (,)*/
+		putchar (' '); /*print a space*/
+	}
+
+	putchar('\n');
+
+	return (0);
+}
